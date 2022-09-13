3ff30d94987a3292394d509eaad5c5d370ec527e
İlaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2022-09-13 14:41:41 +0400

    Update and rename abc.c to abc.cpp

--- abc.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	printf("a \n");
-	printf("bb \n");
-	printf("ccc \n");
-	return 0;
-}
---
+++ abc.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"a"<<endl;
+	cout<<"bb"<<endl;
+	cout<<"ccc"<<endl;
+	return 0;
+}
