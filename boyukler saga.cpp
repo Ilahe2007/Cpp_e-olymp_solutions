4e89e7632684ba6b807a24c95ea13cb28ffbb9f3
İlaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2022-10-29 22:09:37 +0400

    Create boyukler saga.cpp

---
+++ boyukler saga.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long n;
+	cin>>n;
+	long long a[n];
+	for (int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	long long max=a[0];
+	for (int i=1;i<n;i++){
+		if (a[i]>max){
+			max=a[i];
+		}
+	}
+	for (int i=0;i<n;i++){
+		if (a[i]!=max){
+			cout<<a[i]<<" ";
+		}
+	}for (int i=0;i<n;i++){
+		if (a[i]==max){
+			cout<<max<<" ";
+		}
+	}
+}
