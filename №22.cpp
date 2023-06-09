4f45ce25402b920c704ff7440bdff5ab8d9f94cc
İlaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2023-06-09 22:00:01 +0400

    Create №22.cpp

---
+++ №22.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll f(ll n){
+    bool a=true;
+    for(ll i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            a=false;
+            break;
+        }
+    }return a;
+}
+
+ll g(ll n){
+    string a=to_string(n);
+    reverse(a.begin(),a.end());
+    n=stoi(a);
+    return n;
+}
+
+int main(){
+    ll a,b,c=0;
+    cin>>a>>b;
+    for(ll i=a;i<=b;i++){
+        if(f(i)==true and f(g(i))==true and i!=1){
+            c++;
+        }
+    }cout<<c<<endl;
+}
